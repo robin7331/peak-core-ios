1b8d9f39469dc571269b1db0928534ccd70e90c3
Robin Reiter <robin7331@gmail.com>
2016-11-03 09:50:51 +0100

    untrack pods

--- Example/Pods/Target Support Files/Pods-peak-core-ios_Example/Pods-peak-core-ios_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_peak_core_ios_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_peak_core_ios_ExampleVersionString[];
-
--- Example/Pods/Target Support Files/peak-core-ios/peak-core-ios-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double peak_core_iosVersionNumber;
-FOUNDATION_EXPORT const unsigned char peak_core_iosVersionString[];
-
