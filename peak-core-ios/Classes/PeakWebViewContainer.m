5ade416505fe70853787231c1e1e4c633bc9c3f7
Robin Reiter <robin@bitmechanics.de>
2016-12-12 15:49:16 +0100

    added debug reload on 2s long press with 2 fingers

--- peak-core-ios/Classes/PeakWebViewContainer.m
+++ peak-core-ios/Classes/PeakWebViewContainer.m
@@ -7,6 +7,9 @@
 #import "PeakCore.h"
 
 
+@interface PeakWebViewContainer () <UIGestureRecognizerDelegate>
+@end
+
 @implementation PeakWebViewContainer {
     WKWebView *_webView;
 }
@@ -15,7 +18,23 @@
     _webView = [[WKWebView alloc] initWithFrame:self.bounds configuration:core.webViewConfiguration];
     core.webView = _webView;
     [self addSubview:_webView];
+
+    if (core.debug) {
+        UILongPressGestureRecognizer *debugPressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(debugTapped:)];
+        debugPressRecognizer.numberOfTouchesRequired = 2;
+        debugPressRecognizer.minimumPressDuration = 2.0f;
+        debugPressRecognizer.delegate = self;
+
+        [self addGestureRecognizer:debugPressRecognizer];
+    }
+
     return _webView;
+}
+
+-(void)debugTapped:(UILongPressGestureRecognizer *)recognizer {
+    if (_webView) {
+        [_webView reload];
+    }
 }
 
 - (void)layoutSubviews {
